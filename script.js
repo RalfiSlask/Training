92e0a3d45b71364e85922c70d934ef4a71d08ef4
RalfiSlask <nilssonnarki@gmail.com>
2023-11-08 13:30:54 +0100

    8th nov

---
+++ script.js
@@ -0,0 +1,10 @@
+function getElementsAfter(array, n) {
+    // your code here
+    let filteredArray = array.map((element, index) => element = 4);
+    return filteredArray;
+  }
+  
+  let output = getElementsAfter(['a', 'b', 'c', 'd', 'e'], 2); 
+  console.log(output); // --> ['d', 'e']
+  
+let string = 'Gej';
